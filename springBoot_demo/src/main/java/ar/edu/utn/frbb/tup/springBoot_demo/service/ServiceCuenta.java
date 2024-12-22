a758b66730ea641021bc17e4a5219b5c025236ee
Miqueas Correa <miki.mate.tizi@gmail.com>
2024-12-22 15:32:56 -0300

    creo transfer

--- springBoot_demo/src/main/java/ar/edu/utn/frbb/tup/springBoot_demo/service/ServiceCuenta.java
+++ springBoot_demo/src/main/java/ar/edu/utn/frbb/tup/springBoot_demo/service/ServiceCuenta.java
@@ -7,7 +7,6 @@ import ar.edu.utn.frbb.tup.springBoot_demo.model.Cuenta;
 import ar.edu.utn.frbb.tup.springBoot_demo.model.MsjResponce;
 import ar.edu.utn.frbb.tup.springBoot_demo.model.dto.CuentaDto;
 import ar.edu.utn.frbb.tup.springBoot_demo.model.dto.MovimientosDto;
-import ar.edu.utn.frbb.tup.springBoot_demo.model.dto.TransferirDto;
 import ar.edu.utn.frbb.tup.springBoot_demo.model.exception.CuentaAlreadyExistsException;
 import ar.edu.utn.frbb.tup.springBoot_demo.persistence.DaoCuenta;
 import ar.edu.utn.frbb.tup.springBoot_demo.persistence.DaoMovimientos;
@@ -96,62 +95,6 @@ public class ServiceCuenta {
         daoMovimientos.save(movimientoDto1);
 
         return new MsjResponce("EXITOSA", "Se retiró el dinero de la cuenta.");
-    }
-
-    // metodo para transferencia
-    public MsjResponce transferir(TransferirDto transferirDto){
-        Cuenta cuentaOrigen = buscarCuenta(transferirDto.getCuentaOrigen());
-        Cuenta cuentaDestino = buscarCuenta(transferirDto.getCuentaDestino());
-
-        // Sedebe verificar que la moneda de ambas cuentas sea la misma, y en caso de ser
-        // transferencia en pesos, el banco cobrará un cargo del 2% de la transferencia si
-        // supera $1000000 (o caso de ser dólares cobra un 0.5% si son más de U$S5000).
-
-        Double monto = transferirDto.getMonto();
-
-        if (transferirDto.getMoneda() == "pesos"){
-            if (monto > 1000000) {
-                monto = monto - (monto * 0.02);
-            }
-        } else if (transferirDto.getMoneda() == "dolares"){
-            if (monto > 5000) {
-                monto = monto - (monto * 0.005);
-            }
-        }
-        // realizar la transferencia
-        cuentaOrigen.transferencia(cuentaDestino, monto);
-
-        // guardar el moviimiento y actualizar las cuentas
-        CuentaDto cuentaDtoOrigen = new CuentaDto();
-        CuentaDto cuentaDtoDestino = new CuentaDto();
-
-        cuentaDtoOrigen.setTipoCuenta(cuentaOrigen.getTipoCuenta());
-        cuentaDtoOrigen.setMoneda(cuentaOrigen.getMoneda());
-        cuentaDtoOrigen.setTitular(cuentaOrigen.getTitular());
-        cuentaDtoOrigen.setEstaA(cuentaOrigen.getEstaA());
-        cuentaDtoOrigen.setSaldo(cuentaOrigen.getSaldo());
-        cuentaDtoDestino.setTipoCuenta(cuentaDestino.getTipoCuenta());
-        cuentaDtoDestino.setMoneda(cuentaDestino.getMoneda());
-        cuentaDtoDestino.setTitular(cuentaDestino.getTitular());
-        cuentaDtoDestino.setEstaA(cuentaDestino.getEstaA());
-        cuentaDtoDestino.setSaldo(cuentaDestino.getSaldo());
-
-        daoCuenta.update(cuentaDtoOrigen, cuentaOrigen.getNumeroCuenta());
-        daoCuenta.update(cuentaDtoDestino, cuentaDestino.getNumeroCuenta());
-        // crear el movimiento
-        MovimientosDto movimientoDto1 = new MovimientosDto();
-        MovimientosDto movimientoDto2 = new MovimientosDto();
-
-        movimientoDto1.setNumeroCuenta(cuentaOrigen.getNumeroCuenta());
-        movimientoDto1.setDescripcion("Transferencia a: " + cuentaDestino.getNumeroCuenta() + " - Monto: " + transferirDto.getMonto() + "$ " + transferirDto.getMoneda());
-        movimientoDto2.setNumeroCuenta(cuentaDestino.getNumeroCuenta());
-        movimientoDto2.setDescripcion("Transferencia de: " + cuentaOrigen.getNumeroCuenta() + " - Monto: " + transferirDto.getMonto() + "$ " + transferirDto.getMoneda());
-
-        daoMovimientos.save(movimientoDto1);
-        daoMovimientos.save(movimientoDto2);
-
-        // si llego hasta aca sin lanzar una excepcion es que se realizo la transferencia
-        return new MsjResponce("EXITOSA", "La transferencia se realizó correctamente.");
     }
 
     // metodo para dar de alta la cuenta
